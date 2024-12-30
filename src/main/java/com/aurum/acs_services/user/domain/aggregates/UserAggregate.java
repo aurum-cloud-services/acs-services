3b06de51a5001fa86584c4e5694122a87e1611b8
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2024-12-30 20:27:49 -0300

    chore: validate email on validate domain

--- src/main/java/com/aurum/acs_services/user/domain/aggregates/UserAggregate.java
+++ src/main/java/com/aurum/acs_services/user/domain/aggregates/UserAggregate.java
@@ -1,12 +1,16 @@
 package com.aurum.acs_services.user.domain.aggregates;
 
 import com.aurum.acs_services.user.domain.exceptions.CpfIsNotValidException;
+import com.aurum.acs_services.user.domain.exceptions.EmailIsNotValidException;
 import com.aurum.acs_services.user.domain.exceptions.NameIsNotValidException;
 import com.aurum.acs_services.user.domain.exceptions.PasswordIsNotValidException;
 import com.aurum.acs_services.user.domain.validators.CpfValidator;
 import lombok.Getter;
 
 import java.time.LocalDate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import com.aurum.acs_services.shared.domain.validators.DomainValidator;
 
 @Getter
@@ -73,8 +77,13 @@ public class UserAggregate {
     }
 
     private void validateDomain() {
+        String emailRegex = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+        Pattern pattern = Pattern.compile(emailRegex);
+        Matcher matcher = pattern.matcher(email);
+
         DomainValidator.throwsWhen(this.name.length() > 40, NameIsNotValidException.class);
         DomainValidator.throwsWhen(!CpfValidator.isValid(this.cpf), CpfIsNotValidException.class);
         DomainValidator.throwsWhen(this.password.length() < 12 || this.password.length() > 100, PasswordIsNotValidException.class);
+        DomainValidator.throwsWhen(!matcher.matches(), EmailIsNotValidException.class);
     }
 }
