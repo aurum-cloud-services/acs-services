e939525ddcf323607829567bba25fb95bf518021
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2024-12-30 14:12:17 -0300

    chore: setting port by enviroment

--- src/main/java/com/aurum/acs_services/AcsServicesApplication.java
+++ src/main/java/com/aurum/acs_services/AcsServicesApplication.java
@@ -1,5 +1,6 @@
 package com.aurum.acs_services;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AcsServicesApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+
+		String port = dotenv.get("PORT", "14567");
+
+		System.setProperty("server.port", port);
+
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(AcsServicesApplication.class, args);
 	}
 
