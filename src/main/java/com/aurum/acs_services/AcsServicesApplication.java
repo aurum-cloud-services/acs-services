7d14265073ef48fd9fa0a401fc281e8a129c8efd
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2024-12-30 14:16:01 -0300

    chore: connecting to db

--- src/main/java/com/aurum/acs_services/AcsServicesApplication.java
+++ src/main/java/com/aurum/acs_services/AcsServicesApplication.java
@@ -14,6 +14,23 @@ public class AcsServicesApplication {
 
 		System.setProperty("server.port", port);
 
+		String postgresUri = dotenv.get("POSTGRES_URI");
+		String postgresUser = dotenv.get("POSTGRES_USER");
+		String postgresPassword = dotenv.get("POSTGRES_PASSWORD");
+		String postgresDdlMode = dotenv.get("POSTGRES_DDL_MODE", "validate");
+
+		if (postgresUri == null || postgresUri.isEmpty()) throw new RuntimeException("NO POSTGRES URI SET");
+		if (postgresUser == null || postgresUser.isEmpty()) throw new RuntimeException("NO POSTGRES USER SET");
+		if (postgresPassword == null || postgresPassword.isEmpty()) throw new RuntimeException("NO POSTGRES PASSWORD SET");
+
+		System.setProperty("spring.datasource.url", postgresUri);
+		System.setProperty("spring.datasource.username", postgresUser);
+		System.setProperty("spring.datasource.password", postgresPassword);
+		System.setProperty("spring.datasource.driver-class-name", "org.postgresql.Driver");
+
+		System.setProperty("spring.jpa.database-platform", "org.hibernate.dialect.PostgreSQLDialect");
+		System.setProperty("spring.jpa.hibernate.ddl-auto", postgresDdlMode);
+
 		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 
 		SpringApplication.run(AcsServicesApplication.class, args);
