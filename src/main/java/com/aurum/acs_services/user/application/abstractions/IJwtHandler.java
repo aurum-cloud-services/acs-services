b1517fefd6956d25b5600395c6dfa2d6f4a3f6c6
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2024-12-30 19:48:24 -0300

    feat: isValid token verification

--- src/main/java/com/aurum/acs_services/user/application/abstractions/IJwtHandler.java
+++ src/main/java/com/aurum/acs_services/user/application/abstractions/IJwtHandler.java
@@ -1,7 +1,8 @@
-package com.aurum.acs_services.shared.application.abstractions;
+package com.aurum.acs_services.user.application.abstractions;
 
 import com.aurum.acs_services.user.domain.aggregates.UserAggregate;
 
 public interface IJwtHandler {
     String encode(UserAggregate user);
+    Boolean isValid(String token);
 }
--- src/main/java/com/aurum/acs_services/user/infrastructure/handlers/JwtHandler.java
+++ src/main/java/com/aurum/acs_services/user/infrastructure/handlers/JwtHandler.java
@@ -1,9 +1,11 @@
-package com.aurum.acs_services.shared.infrastructure.handlers;
+package com.aurum.acs_services.user.infrastructure.handlers;
 
-import com.aurum.acs_services.shared.application.abstractions.IJwtHandler;
+import com.aurum.acs_services.user.application.abstractions.IJwtHandler;
 import com.aurum.acs_services.user.domain.aggregates.UserAggregate;
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -26,5 +28,19 @@ public class JwtHandler  implements IJwtHandler {
                 .withIssuedAt(new Date())
                 .withExpiresAt(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
                 .sign(algorithm);
+    }
+
+    @Override
+    public Boolean isValid(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secretKey);
+            JWTVerifier verifier = JWT.require(algorithm).build();
+
+            verifier.verify(token);
+
+            return true;
+        } catch (JWTVerificationException e) {
+            return false;
+        }
     }
 }
