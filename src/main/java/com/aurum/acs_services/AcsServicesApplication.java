77d75d712eac5b4753761c180dcad9c8e0c82e91
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2024-12-30 14:02:12 -0300

    feat: initial commit

---
+++ src/main/java/com/aurum/acs_services/AcsServicesApplication.java
@@ -0,0 +1,13 @@
+package com.aurum.acs_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcsServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcsServicesApplication.class, args);
+	}
+
+}
